0a3fa6f27903f9cf2530250b808fd422f2953dd9
Chris Gebert <cj@chrisgebert.org>
2021-08-01 14:05:56 -0400

    add functionality to parse html of source page

--- get-cord-19-publications.py
+++ get-cord-19-publications.py
@@ -4,10 +4,22 @@
 
 # %% required library
 import pandas as pd
+import datetime
+import requests
+from bs4 import BeautifulSoup
 
-# %% CORD-19 Dataset
-cord_19 = pd.read_csv('https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2021-07-26/metadata.csv', parse_dates=['publish_time'])
+# CORD-19 Historical Releases
+url = 'https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/historical_releases.html'
 
+r = requests.get(url)
+soup = BeautifulSoup(r.text)
+latest_date = soup.td.get_text()
+
+# create date column
+date = str(datetime.date.today())
+
+# %% Load dataframe of metadata from CORD-19 Dataset
+cord_19 = pd.read_csv('https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/' + latest_date + '/metadata.csv', parse_dates=['publish_time'])
 titles = cord_19[['title', 'doi', 'publish_time']]
 
 # Drop NaN publish_time values and group by date of publication
