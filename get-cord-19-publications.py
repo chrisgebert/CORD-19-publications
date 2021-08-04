12acbf00cec98e1cc4bea49bd6b5247de9d5da9d
Chris Gebert <cj@chrisgebert.org>
2021-08-04 09:02:56 -0400

    add functionality to read and process latest data

--- get-cord-19-publications.py
+++ get-cord-19-publications.py
@@ -2,27 +2,32 @@
 #!/usr/bin/env python3
 # 
 
-# %% required library
+# %% required libraries
 import pandas as pd
 import datetime
 import requests
 from bs4 import BeautifulSoup
 
-# CORD-19 Historical Releases
+# Get date of latest release from CORD-19 Historical Releases
 url = 'https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/historical_releases.html'
-
 r = requests.get(url)
 soup = BeautifulSoup(r.text)
 latest_date = soup.td.get_text()
 
-# create date column
-date = str(datetime.date.today())
+# Create date column
+date = str(datetime.date.today() - datetime.timedelta(days = 1))
+describe = ['Date', 'Article Count', 'Mean Author Count', 'Std Author Count', 'Min Author Count', '25% Percentile', '50% Percentile', '75% Percentile', 'Max Author Count']
 
-# %% Load dataframe of metadata from CORD-19 Dataset
+# Load dataframe of metadata from CORD-19 Dataset and process author data
 cord_19 = pd.read_csv('https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/' + latest_date + '/metadata.csv', parse_dates=['publish_time'])
-titles = cord_19[['title', 'doi', 'publish_time']]
+cord_19['author_list'] = cord_19['authors'].astype(str).apply(lambda x: x.split(';')
+cord_19['author_count'] = cord_19['author_list'].str.len()
+row = cord_19['author_count'].describe().tolist()
+row.insert(0, latest_date)
+latest_data = dict(zip(describe, row)
 
-# Drop NaN publish_time values and group by date of publication
-publish_time_grouped = titles[titles['publish_time'].notnull()].groupby('publish_time').count()['title'].reset_index()
+# Load previously collected data, append latest, and save file to repo
+historical_data = pd.read_csv('data/CORD-19-historical-releases.csv').reset_index()
+historical_data = historical_data.append(latest_data, ignore_index=True)
+historical_data.to_csv('data/CORD-19-historical-releases.csv', index=False)
 
-publish_time_grouped.sort_values('title', ascending=False).to_csv('data/cord_19-publications-by-date.csv', index=False)
