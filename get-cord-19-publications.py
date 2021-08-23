3d7ea105eb4a7ef1932088b3201a25bf4fc250ec
Chris Gebert <cj@chrisgebert.org>
2021-08-23 19:31:18 -0400

    Update get-cord-19-publications.py
    add conditional for existing data

--- get-cord-19-publications.py
+++ get-cord-19-publications.py
@@ -1,6 +1,6 @@
-#####################################################################
+########################################################################
 #!/usr/bin/env python3
-# 
+######################################################################## 
 
 # %% required libraries
 import pandas as pd
@@ -18,15 +18,17 @@ latest_date = soup.td.get_text()
 date = str(datetime.date.today() - datetime.timedelta(days = 1))
 describe = ['Date', 'Article Count', 'Mean Author Count', 'Std Author Count', 'Min Author Count', '25% Percentile', '50% Percentile', '75% Percentile', 'Max Author Count']
 
-# Load dataframe of metadata from CORD-19 Dataset and process author data
-cord_19 = pd.read_csv('https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/' + latest_date + '/metadata.csv', parse_dates=['publish_time'])
-cord_19['author_list'] = cord_19['authors'].astype(str).apply(lambda x: x.split(';'))
-cord_19['author_count'] = cord_19['author_list'].str.len()
-row = cord_19['author_count'].describe().tolist()
-row.insert(0, latest_date)
-latest_data = dict(zip(describe, row))
-
-# Load previously collected data, append latest, and save file to repo
+# Check if latest dataset date is not in historical data file
 historical_data = pd.read_csv('data/CORD-19-historical-releases.csv')
-historical_data = historical_data.append(latest_data, ignore_index=True).sort_values('Date', ascending=False)
-historical_data.to_csv('data/CORD-19-historical-releases.csv', index=False)
+
+if latest_date != historical_data.loc[0]['Date']:
+  # Load dataframe of metadata from CORD-19 Dataset and process author data
+  cord_19 = pd.read_csv('https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/' + latest_date + '/metadata.csv', parse_dates=['publish_time'])
+  cord_19['author_list'] = cord_19['authors'].astype(str).apply(lambda x: x.split(';'))
+  cord_19['author_count'] = cord_19['author_list'].str.len()row = cord_19['author_count'].describe().tolist()
+  row.insert(0, latest_date)
+  latest_data = dict(zip(describe, row))
+  
+  # Append latest data and write file to repo
+  historical_data = historical_data.append(latest_data, ignore_index=True).sort_values('Date', ascending=False)
+  historical_data.to_csv('data/CORD-19-historical-releases.csv', index=False)
